4e4f1d0dd7644dfa629af403fea6debb48c09c99
Miguel Lopez <ing.miguell@gmail.com>
2023-05-12 14:55:47 -0600

    add try - except when the user inputs numbers instead characters

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ import pandas
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
-phonetic_dictionary = {row.letter:row.code for index, row in data.iterrows()}
+phonetic_dictionary = {row.letter: row.code for index, row in data.iterrows()}
 
-word = input("Enter a word: ").upper()
 
-phonetic_code = [phonetic_dictionary[letter] for letter in word]
-print(phonetic_code)
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        phonetic_code = [phonetic_dictionary[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please")
+        generate_phonetic()
+    else:
+        print(phonetic_code)
+
+
+generate_phonetic()
